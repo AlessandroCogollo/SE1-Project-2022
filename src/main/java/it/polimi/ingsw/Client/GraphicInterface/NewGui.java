ae3a465bf7fff14b6c93e387a2f0000b48b39f9a
frigobar123 <alessandro.cogollo@mail.polimi.it>
2022-05-26 12:50:20 +0200

    Updated maingame.fxml

--- src/main/java/it/polimi/ingsw/Client/GraphicInterface/NewGui.java
+++ src/main/java/it/polimi/ingsw/Client/GraphicInterface/NewGui.java
@@ -135,6 +135,7 @@ public class NewGui extends Application implements Graphic {
         try {
             this.parent = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("scenes/launch.fxml")));
             Scene scene = new Scene(parent);
+            scene.getStylesheets().add("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
             stage.initStyle(StageStyle.UNDECORATED);
             stage.setScene(scene);
             if(currentRoot == 4){
