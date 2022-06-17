0ce7712a15cab6555585da81b48f682d636218dc
Mattia-Colbertaldo <mattia.colbertaldo@mail.polimi.it>
2022-06-17 16:31:54 +0200

    gui update

--- src/main/java/it/polimi/ingsw/Client/GraphicInterface/FXMLController/MainGameController.java
+++ src/main/java/it/polimi/ingsw/Client/GraphicInterface/FXMLController/MainGameController.java
@@ -2,10 +2,14 @@ package it.polimi.ingsw.Client.GraphicInterface.FXMLController;
 
 import it.polimi.ingsw.Client.DataCollector;
 import it.polimi.ingsw.Client.GraphicInterface.Gui;
+import it.polimi.ingsw.Message.ModelMessage.CharacterSerializable;
 import it.polimi.ingsw.Message.ModelMessage.CloudSerializable;
+import it.polimi.ingsw.Server.Model.Characters.*;
+import it.polimi.ingsw.Server.Model.Characters.Character;
 import it.polimi.ingsw.Server.Model.Cloud;
 import it.polimi.ingsw.Server.Model.Island;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
@@ -42,15 +46,11 @@ public class MainGameController extends Controller{
     @FXML
     private Label username3;
     @FXML
-    private Label description3;
-    @FXML
     private Rectangle rectangle4;
     @FXML
     private Rectangle color4;
     @FXML
     private Label username4;
-    @FXML
-    private Label description4;
 
     @FXML
     private Tab tab1;
@@ -78,6 +78,32 @@ public class MainGameController extends Controller{
     @FXML
     private GridPane cloudGrid2;
 
+    @FXML
+    private Tab charactersTab;
+
+    @FXML
+    private Label label1;
+    @FXML
+    private Label label2;
+    @FXML
+    private Label label3;
+    @FXML
+    private Label label4;
+
+    @FXML
+    private Rectangle coinsRectangle;
+    @FXML
+    private Label coinsLabel;
+    @FXML
+    private Label coinsNumberLabel;
+    @FXML
+    private ImageView coinsImage;
+
+    @FXML
+    private Label stepsLabel;
+    @FXML
+    private Label playedCharacterLabel;
+
 
     private int numOfPlayers;
 
@@ -92,14 +118,45 @@ public class MainGameController extends Controller{
             this.numOfPlayers = this.dataCollector.getNumOfPlayers();
             setUsernames();
             disableUnused();
+            disableIfDreamMode();
             glowCurrent();
             setClouds();
             setSchools();
             setIslands();
+            setGameStatus();
         } catch (InterruptedException e) {
             e.printStackTrace();
             Thread.currentThread().interrupt(); //reset flag
         }
+    }
+
+    public void disableIfDreamMode(){
+        this.coinsRectangle.setVisible(false);
+        this.coinsImage.setVisible(false);
+        this.coinsImage.setDisable(true);
+        this.coinsLabel.setVisible(false);
+        this.coinsNumberLabel.setVisible(false);
+        this.charactersTab.setDisable(true);
+    }
+
+    public void setGameStatus(){
+        String text = null;
+        switch(dataCollector.getModel().getActiveCharacterId()){
+            case 0 -> text = "Apothecary";
+            case 1 -> text = "Bard";
+            case 2 -> text = "Cleric";
+            case 3 -> text = "Cook";
+            case 4 -> text = "Drunkard";
+            case 5 -> text = "Herald";
+            case 6 -> text = "Jester";
+            case 7 -> text = "Knight";
+            case 8 -> text = "Minotaur";
+            case 9 -> text = "Postman";
+            case 10 -> text = "Princess";
+            case 11 -> text = "Thief";
+            default -> text = "No active character";
+        }
+        playedCharacterLabel.setText(text);
     }
 
 
@@ -108,14 +165,18 @@ public class MainGameController extends Controller{
         this.usernames = dataCollector.getUsernames();
         username1.setText(usernames.get(0));
         tab1.setText(usernames.get(0));
+        label1.setText(dataCollector.getWizards().get(0).name());
         username2.setText(usernames.get(1));
         tab2.setText(usernames.get(1));
+        label2.setText(dataCollector.getWizards().get(1).name());
         if(this.dataCollector.getNumOfPlayers()>2){
             username3.setText(usernames.get(2));
             tab3.setText(usernames.get(2));
+            label3.setText(dataCollector.getWizards().get(2).name());
             if(this.dataCollector.getNumOfPlayers()>3){
                 username4.setText(usernames.get(3));
                 tab4.setText(usernames.get(3));
+                label4.setText(dataCollector.getWizards().get(3).name());
             }
         }
     }
@@ -126,7 +187,7 @@ public class MainGameController extends Controller{
             this.color4.setDisable(true);
             this.rectangle4.setVisible(false);
             this.username4.setVisible(false);
-            this.description4.setVisible(false);
+            this.label4.setVisible(false);
             this.tab4.setDisable(true);
         }
         if(numOfPlayers < 3){
@@ -134,7 +195,7 @@ public class MainGameController extends Controller{
             this.color3.setDisable(true);
             this.rectangle3.setVisible(false);
             this.username3.setVisible(false);
-            this.description3.setVisible(false);
+            this.label3.setVisible(false);
             this.tab3.setDisable(true);
         }
 
@@ -163,6 +224,25 @@ public class MainGameController extends Controller{
     }
 
     public void setClouds(){
+        int num = 0;
+        try {
+            num = this.dataCollector.getNumOfPlayers();
+            System.out.println("Num of players: " + num);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if(num < 4){
+            cloudAnchor4.setDisable(true);
+            cloudAnchor4.setVisible(false);
+            cloudGrid4.setDisable(true);
+            cloudGrid4.setVisible(false);
+            if(num < 3){
+                cloudAnchor3.setDisable(true);
+                cloudAnchor3.setVisible(false);
+                cloudGrid3.setDisable(true);
+                cloudGrid3.setVisible(false);
+            }
+        }
         this.clouds = (ArrayList<CloudSerializable>) this.dataCollector.getModel().getCloudList();
         Integer i = 0;
         ArrayList<GridPane> grids = new ArrayList<>();
@@ -172,25 +252,20 @@ public class MainGameController extends Controller{
         grids.add(this.cloudGrid4);
         int[] colors;
         Random rand = new Random(372);
-        ArrayList<Image> colorImages = new ArrayList<>();
-        colorImages.add(new Image("Raw/Animali/Struzzo.jpg"));
-        colorImages.add(new Image("Raw/Animali/Volpe.jpg"));
-        colorImages.add(new Image("Raw/Animali/Lucertola.jpg"));
-        colorImages.add(new Image("Raw/Animali/Tartaruga.jpg"));
-        colorImages.add(new Image("Raw/Animali/Ghepardo.jpg"));
-        double height = this.cloudAnchor1.getHeight()/this.cloudGrid1.getRowCount();
-        for(GridPane grid : grids){
-            for(CloudSerializable cloud : clouds){
-                colors = cloud.getDrawnStudents();
+        double height = this.cloudGrid1.getPrefHeight()/this.cloudGrid1.getRowCount();
+        height = height/4;
+        System.out.println("Height = " + height);
+        for(int cloud = 0; cloud< clouds.size(); cloud++){
+                colors = clouds.get(cloud).getDrawnStudents();
                 for(int color : colors){
-                    ImageView img = new ImageView(colorImages.get(color));
-                    img.setFitHeight(height);
-                    img.setFitWidth(height);
-                    grid.add(img, rand.nextInt(grid.getColumnCount()), rand.nextInt(grid.getRowCount()));
-                    }
+                    int column = rand.nextInt(grids.get(cloud).getColumnCount());
+                    int row = rand.nextInt(grids.get(cloud).getRowCount());
+                    Circle c = new Circle(height);
+                    c.setFill(convertColor(color));
+                    grids.get(cloud).add(c, column, row);
+                    System.out.println("Added color[" + color + "] student in cloud " + cloud + " at column " + column + ", row " + row);
                 }
             }
-
         }
 
     public Color convertColor(int id){
